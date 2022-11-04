2e58217fda7966b616615157d0c8c7a5333edf83
nixom9733 <116334445+nixom9733@users.noreply.github.com>
2022-11-04 22:06:55 +0900

    nixom9733
    Diaog,자동차 삼면도 이미지, 버튼 이미지 조정했습니다.

---
+++ pyqt/Moring.py
@@ -0,0 +1,49 @@
+import sys
+import urllib.request
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("Moring.ui")[0]
+
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+        self.setGeometry(1300, 1500, 1400, 1300)
+
+        self.Button_test.clicked.connect(self.loadImageFromFile2)
+        self.Button_test2.clicked.connect(self.deleteImage)
+
+        self.qPixmapFileVar = QPixmap()
+        self.qPixmapFileVar.load("Moring_test.jpeg")
+        self.qPixmapFileVar = self.qPixmapFileVar.scaled(1100, 669)
+        self.label_test.setPixmap(self.qPixmapFileVar)
+
+        self.label_test.setGeometry(190, 30, 1181, 669)
+        self.Button_test.setGeometry(670, 750, 100, 100) # x,y,가로 세로
+        self.Button_test2.setGeometry(870, 750, 100, 100)
+
+
+
+    def loadImageFromFile2(self):
+     # QPixmap 객체 생성 후 이미지 파일을 이용하여 QPixmap에 사진 데이터 Load하고, Label을 이용하여 화면에 표시
+          qPixmapFileVar = QPixmap()
+          qPixmapFileVar.load("testImage.png")
+          qPixmapFileVar = qPixmapFileVar.scaled(100, 100)
+          self.right_light.setPixmap(qPixmapFileVar)
+
+    def deleteImage(self):
+        qPixmapFileVar = QPixmap()
+        qPixmapFileVar.load("testImage2.png")
+        qPixmapFileVar = qPixmapFileVar.scaled(100, 100)
+        self.right_light.setPixmap(qPixmapFileVar)
+
+
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
