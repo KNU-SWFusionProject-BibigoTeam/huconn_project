478dbbc9bf88511a699b3cd9c15ba0cbdd75cf74
nixom9733 <116334445+nixom9733@users.noreply.github.com>
2022-11-05 00:28:10 +0900

    nixom9733
    On/OFF 버튼 하나로 통일하고 전구 꺼지는 이미지 뺐습니다.

--- pyqt/Moring.py
+++ pyqt/Moring.py
@@ -7,14 +7,15 @@ from PyQt5 import uic
 
 form_class = uic.loadUiType("Moring.ui")[0]
 
+
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
         self.setGeometry(1300, 1500, 1400, 1300)
 
+        self.Button_test.setCheckable(True)
         self.Button_test.clicked.connect(self.loadImageFromFile2)
-        self.Button_test2.clicked.connect(self.deleteImage)
 
         self.qPixmapFileVar = QPixmap()
         self.qPixmapFileVar.load("Moring_test.jpeg")
@@ -23,22 +24,23 @@ class WindowClass(QMainWindow, form_class) :
 
         self.label_test.setGeometry(190, 30, 1181, 669)
         self.Button_test.setGeometry(670, 750, 100, 100) # x,y,가로 세로
-        self.Button_test2.setGeometry(870, 750, 100, 100)
 
 
 
-    def loadImageFromFile2(self):
+    def loadImageFromFile2(self,state):
      # QPixmap 객체 생성 후 이미지 파일을 이용하여 QPixmap에 사진 데이터 Load하고, Label을 이용하여 화면에 표시
+
+     if self.Button_test.isChecked():
+          self.Button_test.setText("OFF")
           qPixmapFileVar = QPixmap()
           qPixmapFileVar.load("testImage.png")
           qPixmapFileVar = qPixmapFileVar.scaled(100, 100)
           self.right_light.setPixmap(qPixmapFileVar)
-
-    def deleteImage(self):
-        qPixmapFileVar = QPixmap()
-        qPixmapFileVar.load("testImage2.png")
-        qPixmapFileVar = qPixmapFileVar.scaled(100, 100)
-        self.right_light.setPixmap(qPixmapFileVar)
+     else:
+         self.Button_test.setText("ON")
+         qPixmapFileVar = QPixmap()
+         qPixmapFileVar.load("")
+         self.right_light.setPixmap(qPixmapFileVar)
 
 
 
