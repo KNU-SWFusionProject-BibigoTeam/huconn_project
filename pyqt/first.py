1e6ea66eb745815a71adf18bc5ba05fd150e1cfb
nixom9733 <116334445+nixom9733@users.noreply.github.com>
2022-11-18 11:23:32 +0900

    nixom9733
    first

--- pyqt/first.py
+++ pyqt/first.py
@@ -1,6 +1,9 @@
 import sys
+
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import second
 from second import secondwindow
 
 
@@ -14,12 +17,20 @@ class MainWindow(QMainWindow,form_main):
 
 
 
+
+
    def initUI(self):
        self.setupUi(self)
        self.pushButton.clicked.connect(self.button_Second)
+       self.base = QPixmap()
+       self.base.load("DoorAndWindowOpen.png")
+       self.base = self.base.scaled(851, 531)
+       self.insideCar.setPixmap(self.base)
+
+
+
 
    def button_Second(self):
-       self.close()
        self.second = secondwindow()
        self.second.exec_()
        self.show()
